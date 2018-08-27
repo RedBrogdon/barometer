b21f1ed5b55e5f118042d3a9c3bb38962159502a
Emily Fortuna <efortuna@google.com>
2018-08-27 16:03:24 -0700

    Temporarily remove the barometer stream API for the sake of publishing a consistent API for iOS and Android to start with.

--- lib/barometer.dart
+++ lib/barometer.dart
@@ -6,10 +6,6 @@ class Barometer {
   static const MethodChannel _channel =
       const MethodChannel('barometer');
 
-  static const EventChannel _eventChannel = const EventChannel('pressureStream');
-
-  static Stream<double> _pressureStream;
-
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
@@ -18,15 +14,6 @@ class Barometer {
   static Future<double> get reading async {
     final double reading = await _channel.invokeMethod('getBarometer');
     return reading;
-  }
-
-  static Stream<double> get pressureStream {
-    if (_pressureStream == null) {
-      _pressureStream = _eventChannel.receiveBroadcastStream().map<double>((value) => value);
-
-    }
-
-    return _pressureStream;
   }
 
   static Future<bool> initialize() async {
